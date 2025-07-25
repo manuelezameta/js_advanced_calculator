f81293b8c7634bf3ccb24be7db92089d7ced0605
Manuel Lezameta <manuelezameta@gmail.com>
2025-07-25 11:17:01 -0500

    Added implementation of makeCalculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,35 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    add(x) {
+      return this.result + x;
+    },
+    subtract(x) {
+      return this.result - x;
+    },
+    multiply(x) {
+      return this.result * x;
+    },
+    divide(x) {
+      if (x === 0) {
+        throw new Error('Division by zero is not allowed');
+      }
+
+      return this.result / x;
+    },
+    operate(operation, x) {
+      this.result = operation.call(this, x);
+
+      return this;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
